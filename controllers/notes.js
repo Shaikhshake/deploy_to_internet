d0c21b231f95ccd86e34f3750d27b5deef08e32e
ashaikh <ammarshaikh1579@gmail.com>
2025-08-05 15:34:40 +0530

    changed routes to use async/await keywords

--- controllers/notes.js
+++ controllers/notes.js
@@ -2,64 +2,91 @@ const notesRouter = require('express').Router()
 const Note = require('../models/note')
 
 
-notesRouter.get('/', (request, response, next) => {
-    Note.find({})
-        .then(notes => {
-            response.json(notes)
-        })
-        .catch(error => next(error))
+notesRouter.get('/', async (request, response, next) => {
+    const note = await Note.find({})
+    response.json(note)
+    // Note.find({})
+    //     .then(notes => {
+    //         response.json(notes)
+    //     })
+    //     .catch(error => next(error))
 })
 
-notesRouter.get("/:id", (request, response, next) => {
-    Note.findById(request.params.id)
-        .then(note => {
-            if (note){
-                response.json(note)
-            }
-            else {
-                response.status(404).end()
-            }
-        })
-        .catch(error => next(error))
+notesRouter.get("/:id", async (request, response, next) => {
+
+    const note = await Note.findById(request.params.id)
+
+    note? 
+        response.json(note)
+        :response.status(404).end()
+
+
+    // Note.findById(request.params.id)
+    //     .then(note => {
+    //         if (note){
+    //             response.json(note)
+    //         }
+    //         else {
+    //             response.status(404).end()
+    //         }
+    //     })
+    //     .catch(error => next(error))
 })
 
-notesRouter.post("/", (request, response, next) => {
+notesRouter.post("/", async (request, response, next) => {
     const body = request.body
     const newNote = new Note({
         content: body.content,
         important: body.important || false,
     })
-    newNote.save()
-        .then(savedNote => response.json(savedNote))
-        .catch(error => next(error))
+
+    const savedNote = await newNote.save()
+    response.json(savedNote)
+    
+    // newNote.save()
+    //     .then(savedNote => response.json(savedNote))
+    //     .catch(error => next(error))
 })
 
-notesRouter.delete('/:id', (request, response, next) => {
-    Note.findByIdAndDelete(request.params.id)
-        .then(() => {
-            response.status(204).end()
-        })
-        .catch(error => next(error))
+notesRouter.delete('/:id', async (request, response, next) => {
+
+    await Note.findByIdAndDelete(request.params.id)
+    response.status(204).end()
+    // Note.findByIdAndDelete(request.params.id)
+    //     .then(() => {
+    //         response.status(204).end()
+    //     })
+    //     .catch(error => next(error))
 })
 
-notesRouter.put("/:id", (request, response, next) => {
+notesRouter.put("/:id", async  (request, response, next) => {
     const noteID = request.params.id
     const {content, important} = request.body
-    Note.findById(noteID)
-        .then(note => {
-            if (!note){
-                return response.status(404).end()
-            }
+
+    const note = await Note.findById(noteID)
+    if (!note) return response.status(404).end()
+    else {
+        note.content = content
+        note.important = important || false
+        const savedNote = await note.save()
+        response.json(savedNote)
+    }
+
+    // Note.findById(noteID)
+    //     .then(note => {
+    //         if (!note){
+    //             return response.status(404).end()
+    //         }
             
-            note.content = content
-            note.important = important || false
+    //         note.content = content
+    //         note.important = important || false
             
-            note.save()
-                .then(savedNote => {
-                    response.json(savedNote)
-                })
-                .catch(error => next(error))
-        })
+    //         note.save()
+    //             .then(savedNote => {
+    //                 response.json(savedNote)
+    //             })
+    //             .catch(error => next(error))
+    //     })
 })
 
 module.exports = notesRouter
--- tests/note_api.test.js
+++ tests/note_api.test.js
@@ -1,11 +1,37 @@
-const {test, after} = require('node:test')
+const {test, after, beforeEach} = require('node:test')
 const assert = require('node:assert')
 const mongoose = require("mongoose")
 const supertest = require('supertest')
 const app = require('../app')
+const Note = require('../models/note')
+const note = require('../models/note')
+
 
 //wraps app object with supertest func into a super-agent object
 const api = supertest(app) 
+
+const initialNotes = [
+    {
+        content: 'something', 
+        important: false, 
+        id: '687cd22f766acdeef10ab4fd'
+    },
+    {
+        content: 'wicked', 
+        important: true, 
+        id: '687cd236766acdeef10ab4ff'
+    }
+
+]
+
+
+beforeEach(async () => {
+    await Note.deleteMany({});
+    let noteObj = new Note(initialNotes[0])
+    await noteObj.save()
+    noteObj = new Note(initialNotes[1])
+    await noteObj.save()
+})
 
 test('notes are returned as json', async function() {
     await api
@@ -14,10 +40,11 @@ test('notes are returned as json', async function() {
         .expect('Content-Type', /application\/json/)
 })
 
+
 test('all notes are returned', async () => {
     const response = await api.get('/api/notes')
 
-    assert.strictEqual(response.body.length, 2)
+    assert.strictEqual(response.body.length, initialNotes.length)
 })
 
 test('wicked is in there', async () => {
